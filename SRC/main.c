637c5e7e87c299be6d969fae43ac93f5fd7d19b4
lair_f <lair_f@epitech.eu>
2014-03-19 16:43:18 +0100

    MEUR

--- SRC/main.c
+++ SRC/main.c
@@ -1,14 +1,14 @@
 /*
 ** main.c for main in /home/richar_2/RTFinal/SRC
-** 
-** Made by 
+**
+** Made by
 ** Login   <richar_2@epitech.net>
-** 
-** Started on  Wed Mar 19 16:07:12 2014 
-** Last update Wed Mar 19 16:07:23 2014 
+**
+** Started on  Wed Mar 19 16:07:12 2014
+** Last update Wed Mar 19 16:42:23 2014 lair_f
 */
 
 int	main()
 {
-  
+  printf("plop\n");
 }
