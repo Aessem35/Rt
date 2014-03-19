122fae9bcebd7ff12c7d4e94e476d4ebbcd9f442
Richar_2 <florentin.richard@epitech.eu>
2014-03-19 16:07:55 +0100

    source

---
+++ SRC/main.c
@@ -0,0 +1,14 @@
+/*
+** main.c for main in /home/richar_2/RTFinal/SRC
+** 
+** Made by 
+** Login   <richar_2@epitech.net>
+** 
+** Started on  Wed Mar 19 16:07:12 2014 
+** Last update Wed Mar 19 16:07:23 2014 
+*/
+
+int	main()
+{
+  
+}
