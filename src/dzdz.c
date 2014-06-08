3c8e801fb0372da9224569a44670765644bc8984
sainto_p <sainto_p@epitech.eu>
2014-06-08 14:15:57 +0200

    calc

---
+++ src/dzdz.c
@@ -0,0 +1,121 @@
+/*
+** calc.c for  in /home/sainto_p/rendu/MUL_2013_rtv1/src
+** 
+** Made by sainto_p
+** Login   <sainto_p@epitech.net>
+** 
+** Started on  Sun Mar 16 19:24:40 2014 sainto_p
+<<<<<<< HEAD
+** Last update Sun Jun  8 14:03:12 2014 sainto_p
+=======
+** Last update Sun Jun  8 14:01:10 2014 boutel_a
+>>>>>>> b594c7f862912cb1c44a2f97f8f112a35fc99b34
+*/
+
+#include <stdio.h>
+#include "rtv1.h"
+
+float	my_lum_color_sphere(t_color color, float cos)
+{
+  color.hexa = 0xFF0000;
+  color.content[0] *= cos;
+  color.content[1] *= cos;
+  color.content[2] *= cos;
+  return (color.hexa);
+}
+
+float	my_lum_color_cylinder(t_color color, float cos)
+{
+  color.hexa = 0xFFFF00;
+  color.content[0] *= cos;
+  color.content[1] *= cos;
+  color.content[2] *= cos;
+  return (color.hexa);
+}
+
+float	my_lum_color_cone(t_color color, float cos)
+{
+  color.hexa = 0x00FF00;
+  color.content[0] *= cos;
+  color.content[1] *= cos;
+  color.content[2] *= cos;
+  return (color.hexa);
+}
+
+float	my_lum_color_plan(t_color color, float cos2)
+{
+  color.hexa = 0x0000FF;
+  color.content[0] *= cos2;
+  color.content[1] *= cos2;
+  color.content[2] *= cos2;
+  return (color.hexa);
+}
+
+
+int	calc(t_img *img, int i, int j)
+{
+<<<<<<< HEAD
+  t_sphere sphere;
+  t_plan plan;
+=======
+  t_sphere oeil;
+>>>>>>> b594c7f862912cb1c44a2f97f8f112a35fc99b34
+  t_color color;
+  t_plan	loup;
+  t_coord vec;
+  float k[5];
+  float	cos;
+<<<<<<< HEAD
+  float	cos_plan;
+=======
+  float cos2;
+  float result;
+>>>>>>> b594c7f862912cb1c44a2f97f8f112a35fc99b34
+
+  vec.x = 100;
+  vec.y = (img->width / 2) - i;
+  vec.z = (img->height / 2) - j;
+  k[0] = calc_sphere(vec);
+  //  k[1] = calc_plan(vec);
+  k[2] = calc_cylinder(vec);
+  k[3] = calc_cone(vec);
+  k[4] = calc_second_cone(vec);
+<<<<<<< HEAD
+  cos = lum_sphere(vec, sphere);
+  cos_plan = lum_plan(vec, plan);
+=======
+  cos = lum_sphere(vec, oeil);
+  cos2 = lum_plan(vec, loup);
+>>>>>>> b594c7f862912cb1c44a2f97f8f112a35fc99b34
+  if (k[0] >= PLAN && ((k[1] >= PLAN && k[0] <= k[1]) || k[1] <= 0))
+    return (result = my_lum_color_sphere(color, cos));
+  if (k[2] >= PLAN && ((k[1] >= PLAN && k[2] <= k[1]) || k[1] <= PLAN))
+    return (result = my_lum_color_cylinder(color, cos));
+  else if (k[3] >= PLAN && ((k[1] >= PLAN && k[3] <= k[1]) || k[1] <= PLAN))
+    return (result = my_lum_color_cone(color, cos));
+  else if (k[4] >= PLAN && ((k[1] >= PLAN && k[4] <= k[1]) || k[1] <= PLAN))
+<<<<<<< HEAD
+    {
+      color.hexa = 0x990099;
+      color.content[0] *= cos;
+      color.content[1] *= cos;
+      color.content[2] *= cos;
+      return (color.hexa);
+    }
+  else if (k[1] > PLAN)
+    {
+      color.hexa = 0x0000FF;
+      color.content[0] *= cos_plan;
+      color.content[1] *= cos_plan;
+      color.content[2] *= cos_plan;
+      return (color.hexa);
+    }
+=======
+  return (result = my_lum_color_cone(color, cos));
+  // else if (k[1] > PLAN)
+  //return (result = my_lum_color_plan(color, cos2));
+>>>>>>> b594c7f862912cb1c44a2f97f8f112a35fc99b34
+  return (0);
+}
+
+
